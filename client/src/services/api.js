7a11f2be834b7504eeedf15ad0430170efe8ac05
Devendra4772 <devendrakumaryadav532@gmail.com>
2026-05-19 11:06:17 +0530

    Updated backend API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://end-backend-vna2.onrender.com',
 });
 
 // Interceptor to add JWT token
