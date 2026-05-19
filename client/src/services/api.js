1502ddc5cd600fff61fbb7081e6edb3b88dbf15a
Devendra4772 <devendrakumaryadav532@gmail.com>
2026-05-19 11:43:01 +0530

    Fixed API route

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://end-backend-vna2.onrender.com',
+  baseURL: 'https://end-backend-vna2.onrender.com/api',
 });
 
-// Interceptor to add JWT token
 api.interceptors.request.use((config) => {
   const user = JSON.parse(localStorage.getItem('user'));
+
   if (user && user.token) {
     config.headers.Authorization = `Bearer ${user.token}`;
   }
+
   return config;
 }, (error) => {
   return Promise.reject(error);
